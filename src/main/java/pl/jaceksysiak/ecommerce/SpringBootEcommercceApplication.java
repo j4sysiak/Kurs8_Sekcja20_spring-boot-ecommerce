87b94fce1f56f6b231826ff63c36ed1b256fc099
Jacek <jaceksysiak@wp.pl>
2020-06-07 06:58:02 +0200

    333

--- src/main/java/pl/jaceksysiak/ecommerce/SpringBootEcommercceApplication.java
+++ src/main/java/pl/jaceksysiak/ecommerce/SpringBootEcommercceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootEcommercceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEcommercceApplication.class, args);
 	}
