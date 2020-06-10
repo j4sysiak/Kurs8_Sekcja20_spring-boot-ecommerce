b45d95f0c31b3fbfb3eb301fa6037a488402ce06
Jacek <jaceksysiak@wp.pl>
2020-06-10 07:41:41 +0200

    jjj

--- src/main/java/pl/jaceksysiak/ecommerce/dao/ProductRepository.java
+++ src/main/java/pl/jaceksysiak/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,15 @@
 package pl.jaceksysiak.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.jaceksysiak.ecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
 }
